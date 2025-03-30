a91e403ab19c3fcf22ac3e7edeb26fba61bf3f8c
cuongluong7913 <cuongluong7913@gmail.com>
2025-03-30 23:48:33 +0700

    Artical and Report comment

---
+++ src/route/Report.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const route = express.Router();
+const ReportController = require("../controller/ReportController");
+
+route.post("/", ReportController.create);
+route.get("/", ReportController.getAll);
+route.delete("/:id", ReportController.remove);
+module.exports = route;
