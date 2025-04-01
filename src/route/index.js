00da175636c6d85577390f4be05cb1b56876f7e5
datmino09 <100676522+datmino09@users.noreply.github.com>
2025-04-01 18:02:21 +0700

    Update index.js

--- src/route/index.js
+++ src/route/index.js
@@ -3,11 +3,13 @@ const CommentRoute = require("./CommentRoute");
 const ReportRoute = require("./ReportRoute");
 const ArticleRoute = require("./ArticlesRoute");
 const CategoryRoute = require("./CategoryRoute");
+const AdminRoute = require("./AdminRoute");
 function routes(app) {
     app.use("/user", UserRoute);
     app.use("/comment",CommentRoute);
     app.use("/report",ReportRoute);
     app.use("/articles",ArticleRoute);
     app.use("/category",CategoryRoute);
+    app.use("/admin",AdminRoute)
   }
 module.exports = routes;
