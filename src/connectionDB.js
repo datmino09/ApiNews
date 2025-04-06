c81bdea4b5c9b03c7f8293a2a58466b1fcf4e845
datmino09 <datmino93737@gmail.com>
2025-04-06 14:21:19 +0700

    update_version_3.0

--- src/connectionDB.js
+++ src/connectionDB.js
@@ -1,25 +1,8 @@
-// const { Sequelize } = require('sequelize');
-
-// const sequelize = new Sequelize(process.env.MYSQL_DBNAME,process.env.MYSQL_USER ,process.env.MYSQL_PASSWORD , {
-//     host: process.env.MYSQL_HOST,
-//     dialect: 'mysql',
-//     logging: false,
-// });
-// const ConnectionDatabase = async () =>{
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Connection has been established successfully.');
-//     } catch (error) {
-//         console.error('Unable to connect to the database:', error);
-//     }
-// }
-// ConnectionDatabase();
-// module.exports = sequelize;
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_news', 'root', '', {
-    host: 'localhost',
-    dialect:'mysql',
+const sequelize = new Sequelize(process.env.MYSQL_DBNAME,process.env.MYSQL_USER ,process.env.MYSQL_PASSWORD , {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
     logging: false,
 });
 const ConnectionDatabase = async () =>{
@@ -32,3 +15,20 @@ const ConnectionDatabase = async () =>{
 }
 ConnectionDatabase();
 module.exports = sequelize;
+// const { Sequelize } = require('sequelize');
+
+// const sequelize = new Sequelize('db_news', 'root', '', {
+//     host: 'localhost',
+//     dialect:'mysql',
+//     logging: false,
+// });
+// const ConnectionDatabase = async () =>{
+//     try {
+//         await sequelize.authenticate();
+//         console.log('Connection has been established successfully.');
+//     } catch (error) {
+//         console.error('Unable to connect to the database:', error);
+//     }
+// }
+// ConnectionDatabase();
+// module.exports = sequelize;
